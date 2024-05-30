0f7a1575d88aedd964693e3fd535017dc56c6f0d
Gaas Maxim <gaasmaksim27@gmail.com>
2024-05-29 17:01:42 -0700

    Fix some bugs

--- social/social/urls.py
+++ social/social/urls.py
@@ -32,31 +32,32 @@ router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'posts', base_views.PostViewSet)
 
+try:
+    urlpatterns = [
+        # path('admin/', admin.site.urls),
+        path('', include('base.urls', namespace='base')),
+        path('', include('users.urls', namespace='users')),
+        path('', include('notification.urls', namespace='notification')),
+        path('api/v1/', include(router.urls)),
+        path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+        path('chat/', include('chat.urls', namespace='chat')),
+        path('sendmail/', mail_views.sending_mail_to_all, name="sendmail"),
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('base.urls', namespace='base')),
-    path('', include('users.urls', namespace='users')),
-    path('', include('notification.urls', namespace='notification')),
-    path('api/v1/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('chat/', include('chat.urls', namespace='chat')),
-    path('sendmail/', mail_views.sending_mail_to_all, name="sendmail"),
+        # Profile/Registration/Login/Logout
+        path('register/', user_views.register, name='register'),
+        path('profile/', user_views.profile, name='profile'),
+        path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+        path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 
-    # Profile/Registration/Login/Logout
-    path('register/', user_views.register, name='register'),
-    path('profile/', user_views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
-
-    # Change password
-    path('change-password/', auth_views.PasswordChangeView.as_view(template_name='registration/change_password_form.html',
-                                                                    success_url=reverse_lazy('change_password_done')),
-                                                                    name='change_password'),
-    path('change-password/done', auth_views.PasswordChangeDoneView.as_view(template_name='registration/change_password_done.html'),
-                                                                            name='change_password_done'),
-]
-
+        # Change password
+        path('change-password/', auth_views.PasswordChangeView.as_view(template_name='registration/change_password_form.html',
+                                                                        success_url=reverse_lazy('change_password_done')),
+                                                                        name='change_password'),
+        path('change-password/done', auth_views.PasswordChangeDoneView.as_view(template_name='registration/change_password_done.html'),
+                                                                                name='change_password_done'),
+    ]
+except:
+    urlpatterns = []
 
 if settings.DEBUG:
     # Media
